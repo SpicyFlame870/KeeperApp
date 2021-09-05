28a09b3789d6f56bffe31ddf736856e8cc1caf3e
Rohan Bansal <rohansp870@gmail.com>
2021-09-05 19:10:49 +0530

    Change fonts

---
+++ build/precache-manifest.ae9c7eff01eaa989e8df1ccefb1f3488.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2a1196701dee1cc88d0e07ce37a1386",
+    "url": "/KeeperApp/index.html"
+  },
+  {
+    "revision": "d17bf316ac3769852482",
+    "url": "/KeeperApp/static/js/2.77531513.chunk.js"
+  },
+  {
+    "revision": "42f0d480040b99f2b74f",
+    "url": "/KeeperApp/static/js/main.b6b738de.chunk.js"
+  },
+  {
+    "revision": "ad92294fb1599dc8ff6b",
+    "url": "/KeeperApp/static/js/runtime-main.ff26de38.js"
+  }
+]);
